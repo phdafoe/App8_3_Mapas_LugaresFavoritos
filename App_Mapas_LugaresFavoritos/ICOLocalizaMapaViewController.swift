00cfaa2c0a2a48e4396b02fb878c997b3276e840
phdafoe <phdafoe@gmail.com>
2016-02-13 18:51:57 +0100

    actualizacion_130216_1851

---
+++ App_Mapas_LugaresFavoritos/ICOLocalizaMapaViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ICOLocalizaMapaViewController.swift
+//  App_Mapas_LugaresFavoritos
+//
+//  Created by User on 13/2/16.
+//  Copyright © 2016 iCologic. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ICOLocalizaMapaViewController: UIViewController {
+    
+    //MARK: - VARIABLES LOCALES
+    
+    
+    
+    
+    
+    //MARK: - IBOUTLET
+    
+    @IBOutlet weak var myMapMK: MKMapView!
+    
+    
+    
+    
+    //MARK: - IBACTION
+    
+    
+    
+    //MARK: - CYCLE LIFE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+
+}
---
+++ App_Mapas_LugaresFavoritos/NOTAS DE DESARROLLO.swift
@@ -0,0 +1,12 @@
+//
+//  NOTAS DE DESARROLLO.swift
+//  App_Mapas_LugaresFavoritos
+//
+//  Created by User on 13/2/16.
+//  Copyright © 2016 iCologic. All rights reserved.
+//
+
+//Primero maquetamos nuestra interfaz grafica
+//Usamos el navigation controller como combinador principal y ya le metemos un BarButtonItem a nuestra barra para navegar entre los distintos controladores de trabajo
+//Creamos pues las clases con las que vamos a trabajar
+//
